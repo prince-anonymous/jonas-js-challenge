068c05f6dbfa950d9846ae281284493b0ba45cdc
prince-anonymous <anonymoustech22555@gmail.com>
2023-06-15 14:49:24 +0100

    jonas challenge

---
+++ jonas.js
@@ -0,0 +1,47 @@
+//challenge 1
+/*const markHeight = 1.69
+const johnHeight = 1.95
+console.log(markHeight, johnHeight);
+const markMass = 78
+const johnMass =92
+console.log(markMass, johnMass);
+
+markBmi =(markMass / markHeight **2)
+console.log(markBmi)
+johnBmi = (johnMass/johnHeight **2)
+console.log(johnBmi)
+const higherBmi = markBmi > johnBmi
+console.log(higherBmi)*/
+
+const markHeight = 1.88
+const johnHeight = 1.76
+console.log(markHeight, johnHeight);
+const markMass = 95
+const johnMass = 85
+console.log(markMass, johnMass);
+
+markBmi =(markMass / markHeight **2)
+console.log(markBmi)
+johnBmi = (johnMass/johnHeight **2)
+console.log(johnBmi)
+const higherBmi = markBmi > johnBmi
+console.log(higherBmi)
+
+//challenge 2
+const two = 'challengeTwo'
+console.log(two)
+
+console.log(markMass, johnMass);
+
+markBmi =(markMass / markHeight **2)
+console.log(markBmi)
+johnBmi = (johnMass/johnHeight **2)
+console.log(johnBmi)
+console.log(markBmi, johnBmi)
+
+if("markBmi is higher than johnBmi"){
+    console.log("markBmi is higher than johnBmi")
+}
+else{
+    console.log(`${johnBmi} is higher than ${markBmi}`)
+}
